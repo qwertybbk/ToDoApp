5b49e914a8a03f8790c6f2beb07a153b9c99255e
전지은 <klara010149@gmail.com>
2022-07-28 00:10:35 +0900

    동작안되는 부분 수정완료

--- app/src/main/java/com/example/todoapp/PlanJ.kt
+++ app/src/main/java/com/example/todoapp/PlanJ.kt
@@ -5,11 +5,57 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_plan_j.*
 
 class PlanJ : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan_j)
+        listview1.choiceMode = ListView.CHOICE_MODE_SINGLE
+        val items = ArrayList<String>()
+        val arrayAdapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_single_choice,
+                items
+        )
+        listview1.adapter = arrayAdapter
+        listview1.setOnItemClickListener { parent, view, position, id ->
+            val check = listview1.checkedItemPosition
+            items[check] = "" + "done"
+            // textview1.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG)
+            Toast.makeText(this, "you can do it", Toast.LENGTH_SHORT).show()
+            arrayAdapter.notifyDataSetChanged()
+        }
+
+        //setTextColor(BLUE)
+
+        button1.setOnClickListener() {
+            items.add("" + editText1.text) //추가
+            arrayAdapter.notifyDataSetChanged() //배열어댑터에게 데이터정보가 변경되었음을 알림
+        }
+
+        /*button2.setOnClickListener(){
+
+            //setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG)
+            arrayAdapter.notifyDataSetChanged()
+        }*/
+
+        button3.setOnClickListener {
+
+            val check = listview1.checkedItemPosition
+            if (check > -1) {
+                items.removeAt(check) //삭제
+                listview1.clearChoices() // 지워진 정보에 대해서 수정이 일어나지 않도록 방지하기위해
+                arrayAdapter.notifyDataSetChanged()
+            }
+        }   //괄호가 밑에 있어서 안되나 해서 추가해봤습니다!
+        button4.setOnClickListener(){
+            items.clear() //초기화
+            arrayAdapter.notifyDataSetChanged()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/todoapp/PlanP.kt
+++ app/src/main/java/com/example/todoapp/PlanP.kt
@@ -13,9 +13,8 @@ import android.os.Bundle
 import android.util.Log
 import android.util.SparseBooleanArray
 import android.view.View
+import android.widget.ListView
 import android.widget.*
-import androidx.core.graphics.toColorInt
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_plan_p.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -26,34 +25,45 @@ class PlanP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan_p)
-
-        listview11.choiceMode =ListView.CHOICE_MODE_SINGLE
-        val items= ArrayList<String>()
+        listview1.choiceMode = ListView.CHOICE_MODE_SINGLE
+        val items = ArrayList<String>()
         val arrayAdapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_single_choice,
-            items
+                this,
+                android.R.layout.simple_list_item_single_choice,
+                items
         )
-        listview11.adapter = arrayAdapter
-        listview11.setOnItemClickListener{parent, view, position, id ->
-            textview11.text = listview11.getItemAtPosition(position)as CharSequence}
+        listview1.adapter = arrayAdapter
+        listview1.setOnItemClickListener { parent, view, position, id ->
+            val check = listview1.checkedItemPosition
+            items[check] = "" + "done"
+            // textview1.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG)
+            Toast.makeText(this, "you can do it", Toast.LENGTH_SHORT).show()
+            arrayAdapter.notifyDataSetChanged()
+        }
 
-        button11.setOnClickListener(){
-            items.add(""+editText11.text) //추가
+        //setTextColor(BLUE)
+
+        button1.setOnClickListener() {
+            items.add("" + editText1.text) //추가
             arrayAdapter.notifyDataSetChanged() //배열어댑터에게 데이터정보가 변경되었음을 알림
         }
 
+        /*button2.setOnClickListener(){
 
-        button33.setOnClickListener(){
-            val check = listview11.checkedItemPosition
-            if(check >-1){
+            //setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG)
+            arrayAdapter.notifyDataSetChanged()
+        }*/
+
+        button3.setOnClickListener {
+
+            val check = listview1.checkedItemPosition
+            if (check > -1) {
                 items.removeAt(check) //삭제
-                listview11.clearChoices() // 지워진 정보에 대해서 수정이 일어나지 않도록 방지하기위해
+                listview1.clearChoices() // 지워진 정보에 대해서 수정이 일어나지 않도록 방지하기위해
                 arrayAdapter.notifyDataSetChanged()
             }
-        }
-
-        button44.setOnClickListener(){
+        }   //괄호가 밑에 있어서 안되나 해서 추가해봤습니다!
+        button4.setOnClickListener(){
             items.clear() //초기화
             arrayAdapter.notifyDataSetChanged()
         }
