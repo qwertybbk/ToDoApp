1fef2578869439c8b8ea7d0ba31c2d5eab52b6c2
전지은 <klara010149@gmail.com>
2022-07-29 09:19:35 +0900

    수정완료

--- app/src/main/java/com/example/todoapp/PlanP.kt
+++ app/src/main/java/com/example/todoapp/PlanP.kt
@@ -69,33 +69,6 @@ class PlanP : AppCompatActivity() {
         }
 
 
-        val mPickTimeBtn = findViewById<Button>(R.id.StartTimeBtn)
-        val ePickTimeBtn = findViewById<Button>(R.id.EndTimeBtn)
-        val starttextView = findViewById<TextView>(R.id.timeStart)
-        val endtextView = findViewById<TextView>(R.id.timeEnd)
-
-        //시작시간 버튼
-        mPickTimeBtn.setOnClickListener {
-            val cal = Calendar.getInstance()
-            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
-                starttextView.text = SimpleDateFormat("HH:mm").format(cal.time)
-            }
-            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
-        }
-
-        //끝나는 시간 버튼
-        ePickTimeBtn.setOnClickListener {
-            val cal = Calendar.getInstance()
-            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
-                endtextView.text = SimpleDateFormat("HH:mm").format(cal.time)
-            }
-            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
-        }
-
     }
 
 }
